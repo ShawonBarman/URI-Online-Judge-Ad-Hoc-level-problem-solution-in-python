c44a28da6ea9e12127ae2c84fe5aa20035b63559
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2021-08-17 12:12:30 +0600

    Division of Nlogonia

---
+++ 1091.py
@@ -0,0 +1,17 @@
+while True:
+    k = int(input())
+    if k == 0:
+        break
+    n, m = map(int, input().split())
+    for i in range(k):
+        x, y = map(int, input().split())
+        if x > n and y > m:
+            print("NE")
+        elif x < n and y > m:
+            print("NO")
+        elif x > n and y < m:
+            print("SE")
+        elif x < n and y < m:
+            print("SO")
+        else:
+            print("divisa")
