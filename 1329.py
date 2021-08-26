66ff0edc82d9e941540bf1f2b74035fe73d35b11
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2021-08-26 11:41:53 +0600

    Head or Tail

---
+++ 1329.py
@@ -0,0 +1,6 @@
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    r = list(map(int, input().split()))[:n]
+    print(f"Mary won {r.count(0)} times and John won {r.count(1)} times")
