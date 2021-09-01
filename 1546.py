3088e360dd8edc45137e401e535f96fb3722221f
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2021-09-01 10:37:33 +0600

    Feedback

---
+++ 1546.py
@@ -0,0 +1,9 @@
+arr = ['', 'Rolien', 'Naej', 'Elehcim', 'Odranoel']
+n = int(input())
+while n != 0:
+    n -= 1
+    k = int(input())
+    while k != 0:
+        k -= 1
+        num = int(input())
+        print(arr[num])
