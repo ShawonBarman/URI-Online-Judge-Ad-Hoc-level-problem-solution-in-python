7e731f03193c8394ccb5809eba90c80d65caa352
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2021-10-19 10:02:49 +0600

    States from the North

---
+++ 2803.py
@@ -0,0 +1,6 @@
+countries = ["roraima", "acre", "amapa", "amazonas", "para", "rondonia", "tocantins"]
+name = input()
+if name in countries:
+    print("Regiao Norte")
+else:
+    print("Outra regiao")
